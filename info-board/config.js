44a327ea220f9f6e3cc3ab0b526ab23cb90e17ce
towamz <towamz.jo@gmail.com>
2026-03-27 18:48:03 +0900

    info-board 新規作成

---
+++ info-board/config.js
@@ -0,0 +1,5 @@
+const MASTER_DATA = [
+    { type: "button", label: "ログインID", value: "sampleLoginUsername" },
+    { type: "button", label: "パスワード", value: "SampleP@ssword" },
+    { type: "href",   label: "yahoo", value: "https://yahoo.co.jp" }
+];
