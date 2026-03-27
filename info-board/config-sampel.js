a3f61b31b9f8716efe4387ebc949a48461f519e3
towamz <towamz.jo@gmail.com>
2026-03-27 19:44:17 +0900

    .gitignore追加

---
+++ info-board/config-sampel.js
@@ -0,0 +1,7 @@
+// config.jsにファイル名を変更して実行してください
+
+const MASTER_DATA = [
+    { type: "button", label: "ログインID", value: "sampleLoginUsername" },
+    { type: "button", label: "パスワード", value: "SampleP@ssword" },
+    { type: "href",   label: "yahoo", value: "https://yahoo.co.jp" }
+];
