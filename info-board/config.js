62a9cb4a786403a5089a37a5b658623921ccea93
towamz <towamz.jo@gmail.com>
2026-03-27 19:47:57 +0900

    remove config.js from tracking

--- info-board/config.js
+++
@@ -1,5 +0,0 @@
-const MASTER_DATA = [
-    { type: "button", label: "ログインID", value: "sampleLoginUsername" },
-    { type: "button", label: "パスワード", value: "SampleP@ssword" },
-    { type: "href",   label: "yahoo", value: "https://yahoo.co.jp" }
-];
